6c800bb3c09da22767941ec36dcb248ed9174507
SW <sean.walshe@tutanota.com>
2021-03-05 20:28:13 +0000

    demonstrate new methods in app

--- app.cpp
+++ app.cpp
@@ -5,18 +5,31 @@
 int main()
 {
 	DS3231 rtc = DS3231(1, 0x68);	// i2c-1 and address 0x68
+
+	// display the initial date and time
+	// and temperature
 	rtc.readDateAndTime();
 	rtc.readTemperature();
-	sleep(0.5);
 
-	// (alarm, hour, minute, day/date, alarm mode)
-	rtc.setAlarm(0, 19, 24, 4);
+	// (alarm number, hour, minute, day/date, alarm mode)
+	rtc.setAlarm(0, 19, 02, 5);
 	
-	if(rtc.isAlarmSet(0))
-		std::cout << "Alarm 1 flag is set" << std::endl;
 	
-	if(rtc.isAlarmSet(1))
-		std::cout << "Alarm 2 flag is set" << std::endl;
+	// demonstrating alarm trigger which flashes LED
+	for(int i=0; i<100; i++)
+	{
+		if(rtc.isAlarmSet(0))
+		{
+			std::cout << "Alarm triggered" << std::endl;
+			rtc.flashLED(17);	// gpio17
+			break;
+		}
+		rtc.readDateAndTime();
+		sleep(1); // wait 1 second
+	}
 	
+	// set the frequency of the SQW output
+	rtc.setSQWFreq(rtc.Freq::MLOW);
+		
 	return 0;
 }
