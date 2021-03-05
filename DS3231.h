a571ed0fc2aa0443e077ec34b2169ef1982c8dd6
SW <sean.walshe@tutanota.com>
2021-03-05 20:26:52 +0000

    added SQW methods

--- DS3231.h
+++ DS3231.h
@@ -2,6 +2,9 @@
 #define _DS3231_H
 
 #include "Device.h"
+#include <string>
+
+using std::string;
 
 
 class DS3231 : public Device
@@ -10,9 +13,14 @@ class DS3231 : public Device
 		int bcd2dec(char b);
 		char dec2bcd(int b);
 		enum CTRLBits {AL1E, AL2E, INTCN, RS1, RS2, CONV, BBSQW, EOSC};
+		
+		void writeSysfs(string path, string fname, string value);
 	
 	public:
-		DS3231(unsigned int bus, unsigned int device);
+		DS3231(unsigned int bus, unsigned int device, bool hasBattery=false);
+		
+		enum Freq { LOW, MLOW, MHIGH, HIGH };
+		
 		virtual void readDateAndTime();
 		virtual void readTemperature();
 		virtual void setTime(int hr, int min, int sec);
@@ -21,6 +29,9 @@ class DS3231 : public Device
 		virtual void writeControlReg(unsigned char bit);
 		virtual void setAlarm(bool which, int hr, int min, int day, bool mode=0);
 		virtual bool isAlarmSet(bool which);
+		virtual void flashLED(int gpioNumber, int n_iters=5);
+		virtual void toggleSQWInt(bool enable);
+		virtual void setSQWFreq(DS3231::Freq f);
 		
 };
 
